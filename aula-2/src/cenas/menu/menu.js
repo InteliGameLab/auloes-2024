0859fde92ac7892ee6409b0360e170c30aaadd15
Leandro672 <leandro.gomes@sou.inteli.edu.br>
2024-03-10 19:51:20 -0300

    feat: adiciona tela de menu e tela final no jogo aula2

--- aula-2/src/cenas/menu/menu.js
+++ aula-2/src/cenas/menu/menu.js
@@ -11,23 +11,29 @@ export default class Menu extends Phaser.Scene {
 
     preload() {
         // Carregamento dos recursos da cena
-        this.load.image("menu", "assets/menu.png");
-        this.load.image("botaoMenu", "assets/botaoMenu.png");
+        this.load.image("menu", "assets/fundo.png");
+        this.load.image("botaoMenu", "assets/botaoJogar.png");
+        this.load.image("Logo", "assets/Logo.png");
     }
 
 
     create() {
         // Adicionando background
-        this.add.image(0, 0, "menu").setOrigin(0);
+        this.add.image(0, 0, "menu").setOrigin(0)
+        .setScale(3,3);
+
+        this.add.image(this.game.renderer.width / 2, this.game.renderer.height * 0.30, "Logo")
+        .setScale(0.7, 0.7);
 
         // Adicionando botão de iniciar a próxima cena
-        this.botaoIniciar = this.add.image(100, 200, "botaoMenu");
+        this.botaoIniciar = this.add.image(640, 560, "botaoMenu")
+        .setScale(0.5, 0.5);
         // Adicionando evento de clique ao botão
         this.botaoIniciar.setInteractive();
         this.botaoIniciar.on("pointerup", this.comecarProximaCena, this);
 
         // Adicionando texto de título
-        this.add.text(100, 100, "Onde Está Wally?", { fontFamily: 'Arial', fontSize: 50, color: '#ffffff' });
+       // this.add.text(100, 100, "Onde Está Wally?", { fontFamily: 'Arial', fontSize: 50, color: '#ffffff' });
     }
 
 
